328d40188314ca27a3a0085b5ad51120c69be236
Chima Precious <ositachima.co@gmail.com>
2019-10-29 14:16:06 +0000

    Create selectors for ease use

--- src/app/ngrx-store.ts
+++ src/app/ngrx-store.ts
@@ -2,11 +2,14 @@ import {
   ActionReducer,
   ActionReducerMap,
   MetaReducer,
-  Store
+  Store,
+  createSelector
 } from '@ngrx/store';
 import { environment } from '../environments/environment';
 import { CartState } from './sections/cart/state/store';
 import { reducer as cartReducer } from './sections/cart/state/reducer';
+import { reducer as homeReducer } from './sections/home/state/reducer';
+
 import { HomeState } from './sections/home/state/store';
 
 export interface AppState {
@@ -26,8 +29,27 @@ export function debug(reducer: ActionReducer<any>): ActionReducer<any> {
 
 export const reducers: ActionReducerMap<AppState> = {
   cartState: cartReducer,
-  homeState: null
+  homeState: homeReducer
 };
+
+const cartState = (state: AppState) => state.cartState;
+const homeState = (state: AppState) => state.homeState;
+export const selectCartItems = createSelector(
+  cartState,
+  state => state.cartItems
+);
+export const selectCartTotal = createSelector(
+  cartState,
+  state => state.totalPrize
+);
+export const selectFilter = createSelector(
+  homeState,
+  state => state.filter
+);
+export const selectProducts = createSelector(
+  homeState,
+  state => state.productsList
+);
 
 export const metaReducers: MetaReducer<AppState>[] = !environment.production
   ? []

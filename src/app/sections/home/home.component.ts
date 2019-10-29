e7e10a7a9125847d9a7657f7a27769c4219bd2df
Chima Precious <ositachima.co@gmail.com>
2019-10-29 14:18:04 +0000

    Update home component

--- src/app/sections/home/home.component.ts
+++ src/app/sections/home/home.component.ts
@@ -1,4 +1,11 @@
 import { Component, OnInit } from '@angular/core';
+import { Subject, Observable } from 'rxjs';
+import { debounce, distinctUntilChanged, debounceTime } from 'rxjs/operators';
+import { Filter } from '../../models/filter';
+import { Store, select } from '@ngrx/store';
+import { AppState, selectProducts, selectFilter } from 'src/app/ngrx-store';
+import { HomeActions } from './state/actions';
+import { HomeState } from './state/store';
 
 @Component({
   selector: 'app-home',
@@ -6,7 +13,19 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./home.component.css']
 })
 export class HomeComponent implements OnInit {
-  constructor() {}
+  $filter = this.store.pipe(select(selectFilter));
+
+  constructor(private store: Store<AppState>) {}
 
   ngOnInit() {}
+
+  ontoggled(showOnlySale: boolean) {
+    this.store.dispatch(
+      HomeActions.filterProducts({ filter: { showOnlySale } })
+    );
+  }
+
+  rangeChanged(range: number) {
+    this.store.dispatch(HomeActions.filterProducts({ filter: { range } }));
+  }
 }
--- src/app/sections/home/home.module.ts
+++ src/app/sections/home/home.module.ts
@@ -2,6 +2,9 @@ import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { HomeComponent } from './home.component';
 import { Routes, RouterModule } from '@angular/router';
+import { MastHeadModule } from 'src/app/widgets/mast-head/mast-head.module';
+import { SidebarModule } from 'src/app/widgets/sidebar/sidebar.module';
+import { SwitchModule } from 'src/app/widgets/switch/switch.module';
 
 const HOME_ROUTES: Routes = [
   {
@@ -12,7 +15,13 @@ const HOME_ROUTES: Routes = [
 
 @NgModule({
   declarations: [HomeComponent],
-  imports: [CommonModule, RouterModule.forChild(HOME_ROUTES)],
+  imports: [
+    CommonModule,
+    MastHeadModule,
+    SidebarModule,
+    SwitchModule,
+    RouterModule.forChild(HOME_ROUTES)
+  ],
   exports: [RouterModule]
 })
 export class HomeModule {}
